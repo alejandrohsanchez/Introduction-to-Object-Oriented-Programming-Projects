bcaffe921718e19446f6b8e994fcfb09cea991f4
alejandrohsanchez <asanchez9112@gmail.com>
2021-09-17 09:43:24 -0700

    Updated file names and uploaded lab 2 documents
    The lab 1 folder contains the same files as "Introduction to Control Flow" and lab 2 folder contains new projects. Refer to PDF for more information about new file functionality.

---
+++ Lab2/Lab2_Pt2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+/*
+Define an array of int. take the starting address of that array
+and use static_cast to convert it into a void*. Write a function
+that takes a void*, a number (indicating a number of bytes), and
+a value (indicating the value to which each byte should be set)
+as arguments. The function should set each byte in the specified
+range to the specified value. Try out the function on your array
+of int.
+*/
+int main() {
+    int arr1[5] = {1, 2, 3, 4, 5};
+
+    // Using static_cast to convert array from in to void*.
+    return 1;
+}
