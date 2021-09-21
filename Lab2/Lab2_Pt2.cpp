863aef883aac29609464d1842714f19e0f07f71e
alejandrohsanchez <asanchez9112@gmail.com>
2021-09-20 21:36:26 -0700

    bruh
    bruh moment

--- Lab2/Lab2_Pt2.cpp
+++ Lab2/Lab2_Pt2.cpp
@@ -10,9 +10,39 @@ as arguments. The function should set each byte in the specified
 range to the specified value. Try out the function on your array
 of int.
 */
+
+/*
+The callTo function takes a void*, a number, and a value as arguments.
+The function sets each byte in the specific range to the specific value.
+*/
+void callTo(void *vp, int byteNum, int byteVal) {
+    int i;
+    cout << "The values in void*:" << endl;
+    for (i = 0; i < byteNum; i++) {
+        *(&vp + (8*i)) = (void*)(i + 1);
+        cout << *(&vp + (8 * i)) << endl;
+    }
+    
+    cout << " " << endl;
+    
+    
+}
+
 int main() {
     int arr1[5] = {1, 2, 3, 4, 5};
 
-    // Using static_cast to convert array from in to void*.
+    // Instantiating a pointer void* that points to int array arr1.
+    void* vp[5];
+    *vp = static_cast <void*>(&arr1);
+    
+    // This calls to the callTo function.
+    callTo(&vp, 5, sizeof(&vp));
+    
+    int i;
+    cout << "The values in int array:" << endl;
+    for (i = 0; i < 5; i++) {
+        cout << arr1[i] << endl;
+    }
+    
     return 1;
 }
