d0453537be67d26213119999fc8f88acbef00c0e
alejandrohsanchez <asanchez9112@gmail.com>
2021-11-10 19:40:37 -0800

    Updated question 1

--- Lab8/question1/q1.cpp
+++ Lab8/question1/q1.cpp
@@ -8,17 +8,27 @@ argument (default this to ' '), and an inline member function called print() to 
 using namespace std;
 
 class charArray {
-    int size = 10;
-    char array[10];
+    char var;
+    enum size {N = 5};
+    char array[N];
 
     public:
+        void setChar(char c) {var = c;}
         // Inline constructor to initialize the array with memset().
-        charArray(char c = ' ') {memset(array, c, size);}
-
-        void print() {for (int i = 0; i < size; i++) {cout << charArray[i] << ' ';} cout << endl;}
+        charArray(char c = ' ') {memset(array, c, N); cout << "Initialized with memset()..." << endl;}
+        
+        // Inline member function.
+        void print() {cout << "Printing '" << var << "' " << N << " time(s):" << endl; for (int i = 0; i < N; i++) {cout << array[i] << ' ';} cout << endl;}
 };
 
 int main() {
+    char character;
+    int numba;
+
+    cout << "Enter a character: "; cin >> character;
+    charArray A(character);
+    A.setChar(character);
+    A.print();
 
     return 1;
 }
