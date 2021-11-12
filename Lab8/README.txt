fca240dc2eff9c314ccbaafabde2adf8a71b21d4
alejandrohsanchez <asanchez9112@gmail.com>
2021-11-11 23:52:50 -0800

    Updated Lab 8 Project. Complete.

---
+++ Lab8/README.txt
@@ -0,0 +1,11 @@
+Problem 1
+compile: g++ q1.cpp -o q1
+run: ./q1
+
+Problem 2
+compile: g++ q2.cpp -o q2
+run: ./q2
+
+Problem 3
+compile: g++ q3.cpp -o q3
+run: ./q3
--- Lab8/question3/q3.cpp
+++ Lab8/question3/q3.cpp
@@ -4,10 +4,97 @@ size of the internal array.
 */
 
 #include <iostream>
+#include <sstream>
+#include <cassert>
+#include <cstring>
+#include <cstddef>
 
 using namespace std;
 
+template<typename T, size_t size>
+class Ar{
+
+    T a[size];
+    
+    public:
+        Ar() {memset(a, 0, size*sizeof(*a));}
+        
+        T& operator[](int b){
+            assert(b >= 0 && b < size);
+            
+            return a[b];
+        }
+
+        //inline member template 
+        friend ostream& //output stream of objects
+        
+        operator << (ostream& io, const Ar <T, size>& ii){
+            for (int i = 0; i < size; i++){
+                io << ii.a[i];
+                
+                if (i != (size - 1)) {
+                    io << ", ";
+                }
+            }
+
+            io << endl;
+            
+            return io;
+        }
+
+        friend istream& //input stream objects 
+
+        operator >> (istream& is, Ar <T, size>& aa){
+            for(int i = 0; i < size; i++) {
+                is >> aa.a[i];
+            }
+
+            return is;
+        }
+
+        //out-of line template
+        #if 0
+        friend ostream&
+        operator << (ostream& io, const Ar <T, size>& ii);
+        friend istream&
+        operator >> (istream& is, Ar <T, size>& aa);
+        #endif
+};
+
+#if 0
+
+template<typename T, size_t size>
+ostream&
+operator << (ostream& io, const Ar <T, size>* ii){
+    for (int i = 0; i < N; i++){
+        io << ii.a[i];
+        if(i != (size - 1)) {
+            io << ", ";
+        }
+    }
+
+    io << endl;
+    return io;
+}
+
+template<typename T, size_t size>
+operator >>(istream& is, Ar <T, size>& aa){
+    for(int i = 0; i < size; i++)
+        is >> aa.a[i];
+    return is;
+}
+
+#endif
+
 int main() {
 
-    return 1;
+    stringstream input("41 65 1324 654 234 54 65");
+    
+    Ar<int, 7> I;
+    
+    input >> I;
+    
+    I[6] = -1;
+    
+    cout << I;
 }
